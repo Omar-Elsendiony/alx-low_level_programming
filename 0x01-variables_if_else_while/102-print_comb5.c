6e800daebe8a01b57f8045b5be92dbfb282a2d46
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-12 00:38:25 +0300

    a

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * Description: The description
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i <= 57; i++)
+	{
+
+		for (j = 48; j <= 56; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (!(i == 57 && j == 56 && k == 57 && l == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
