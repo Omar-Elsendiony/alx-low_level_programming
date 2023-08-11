31b10178d0e071eb54c27e03301e0ee47e5a5307
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-11 22:53:10 +0300

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - starting point
+ * Description: The description
+ * Return: 0
+*/
+int main(void)
+{
+    int i;
+	for (i = 97; i <= 122;i++){
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
