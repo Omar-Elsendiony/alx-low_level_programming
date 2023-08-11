46e5e28fdb1e927db54c010c02b4977958acf1ca
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-12 01:57:03 +0300

    f

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 /**
  * main - starting point
  * Description: The description
@@ -11,22 +12,35 @@ int main(void)
 	int j;
 	int k;
 	int l;
+	int newNumber;
+	int testNumber;
+	int n = 400000;
+	int arr[n];
 
+    for (int i =0 ; i < n;i++)
+        arr[i] = 0;
 	for (i = 48; i <= 57; i++)
 	{
-
 		for (j = 48; j <= 57; j++)
 		{
 			for (k = 48; k <= 57; k++)
 			{
 				for (l = 48; l <= 57; l++)
 				{
+					testNumber = (j-48)*1 + (i-48)*10 + (l-48) * 100 + (k-48) *1000;
+					if (arr[testNumber] == 1){
+						continue;
+					}
+					if (i==k && j==l)
+					    continue;
+					newNumber =  (l-48)*1 + (k-48)*10 + (j-48) * 100 + (i-48) *1000;
+					arr[newNumber] = 1;
 					putchar(i);
 					putchar(j);
 					putchar(' ');
 					putchar(k);
 					putchar(l);
-					if (!(i == 57 && j == 57 && k == 57 && l == 57))
+					if (!(i == 57 && j == 56 && k == 57 && l == 57))
 					{
 						putchar(',');
 						putchar(' ');
