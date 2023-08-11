1037918af837c5b5ca8f8785ea137a2ce82bb02b
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-12 00:04:18 +0300

    e

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * Description: The description
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
