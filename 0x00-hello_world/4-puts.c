dca89566968408ddbc4a726c209a210d4d97f543
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-10 19:17:48 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,22 +3,10 @@
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return:  0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-=======
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)  /* main function */
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
->>>>>>> 63da131b078e5e4c7ca20e81b4200174c6667685
 }
