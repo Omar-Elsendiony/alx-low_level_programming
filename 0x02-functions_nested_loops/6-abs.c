3b77866d5496efe03eba4ad262964cf469be3ddb
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-15 22:16:12 +0300

    d

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute
+ * @c: input
+ * Description: The description
+ * Return: 0
+*/
+int _abs(int c)
+{
+	int res;
+
+	res = (c > 0) ? 0 : -c;
+	return (res);
+}
