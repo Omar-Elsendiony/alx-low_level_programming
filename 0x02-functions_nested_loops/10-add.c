3e499c3e6595579046193cce504d77b449031426
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-15 23:58:58 +0300

    v

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - returns the absolute
+ * @a: input
+ * @b: input
+ * Description: The description
+ * Return: 0
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
