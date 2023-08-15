603f7319c2663787b78560005c11a74d0aff373b
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-15 21:43:27 +0300

    x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "string.h"
 /**
- * print_alphabet - returns the alphabet
+ * print_alphabet_x10 - returns the alphabet
  * Description: The description
  * Return: 0
 */
@@ -10,7 +10,9 @@ void print_alphabet_x10(void)
 	int i, j;
 
 	for (j = 0; j < 10; j++)
+	{
 		for (i = 97; i <= 122; i++)
 			_putchar(i);
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
