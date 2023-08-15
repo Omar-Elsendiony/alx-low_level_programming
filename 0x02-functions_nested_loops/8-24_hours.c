257c8a1bed9433639d3514bee3754bf998ce03b8
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-15 23:19:05 +0300

    c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute
+ * @c: input
+ * Description: The description
+ * Return: 0
+*/
+void jack_bauer(void)
+{
+	int i, j, tens, units;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			units = i % 10;
+			if (units == 0)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else
+			{
+				tens = i / 10;
+				_putchar(tens + '0');
+				_putchar(units + '0');
+			}
+			units = j % 10;
+			if (j % 10 == 0)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else
+			{
+				tens = j / 10;
+				_putchar(tens + '0');
+				_putchar(units + '0');
+			}
+			j = j + 1;
+			_putchar('\n');
+		}
+		i = i + 1;
+	}
+}
