2c74fa0a4672bfe9165aec7cd20d71f48a0b9cba
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-15 21:10:48 +0300

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 #include "string.h"
 /**
- * main - starting point
+ * print_alphabet - returns the alphabet
  * Description: The description
  * Return: 0
 */
-int main(void)
-{
-	int i, length;
-
+void print_alphabet(void){
+    int i;
+    
 	for (i = 65; i < 90; i++)
 		_putchar(i);
 	_putchar('\n');
