a3e5a92b7505c0b1db9cc24e3cf9991545d7edd4
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-15 21:17:35 +0300

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,10 @@
  * Description: The description
  * Return: 0
 */
-void print_alphabet(void){
-    int i;
-    
+void print_alphabet(void)
+{
+	int i;
+
 	for (i = 97; i < 122; i++)
 		_putchar(i);
 	_putchar('\n');
