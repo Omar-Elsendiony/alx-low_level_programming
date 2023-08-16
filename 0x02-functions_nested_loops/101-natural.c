2a79bd4d315f0c810273e035d83587a246767688
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-16 20:44:34 +0300

    v

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * print_times_table - returns the time table
+ * natural - returns the time table
  * Description: The description
  * Return: 0
 */
 void natural(void)
 {
 	int i, j, product;
-
-	
 }
