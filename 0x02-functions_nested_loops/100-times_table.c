ec297fc5304e19177e9924abdc4bb3ce27c66164
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-16 20:19:04 +0300

    g

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,20 +10,26 @@ void print_times_table(int n)
 {
 	int i, j, product;
 
+	if (n > 15 || n < 0)
+		return;
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			product = i * j;
-			if (product > 9)
+			if (j != 0)
 			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
-			}
-			else if (j != 0)
-			{
-				_putchar(' ');
-				_putchar(product + '0');
+				if (product > 9 && product < 100)
+				{
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 100 && product < 999)
+				{
+					_putchar(product / 100 + '0');
+					_putchar((product % 100) / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
 			}
 			else
 			{
