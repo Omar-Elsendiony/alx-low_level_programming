ff2e1d8ddcb3fb9eed85c9be6a7a81a744b6d8f5
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-16 20:38:36 +0300

    v

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -38,11 +38,6 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(product + '0');
 			}
-			if (j != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 		}
 		_putchar('\n');
 	}
