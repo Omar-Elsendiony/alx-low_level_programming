e7d0fa29828f6edf6b714483322d78683e57f28e
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-17 21:06:33 +0300

    v

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "string.h"
+/**
+ * print_square - returns the alphabet
+ * @size: the number of rimes to print '\'
+ * Description: The description
+ * Return: 0
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
