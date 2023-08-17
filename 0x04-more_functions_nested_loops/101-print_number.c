2ea70d09e19b7a572a79dcead088289676e0aa0a
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-18 01:53:18 +0300

    d

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * power10 - returns the alphabet
+ * @exponent: the number of rimes to print '\'
+ * Description: The description
+ * Return: 0
+*/
+int power10(int exponent)
+{
+	int i, result;
+
+	result = 1;
+	for (i = 0; i < exponent; i++)
+	{
+		result = result * 10;
+	}
+	return (result);
+}
+/**
+ * print_number - returns the alphabet
+ * @n: the number of rimes to print '\'
+ * Description: The description
+ * Return: 0
+*/
+void print_number(int n)
+{
+	int size, tempNumber, numberToPrint;
+
+	size = 0;
+	tempNumber = n;
+	while (tempNumber != 0)
+	{
+		tempNumber = tempNumber / 10;
+		size = size + 1;
+	}
+	while (size > 1)
+	{
+		numberToPrint = (n % power10(size)) / power10((size - 1));
+		size = size - 1;
+		_putchar(numberToPrint + '0');
+	}
+	_putchar(n % 10 + '0');
+}
