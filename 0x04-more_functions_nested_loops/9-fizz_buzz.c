4490efc1f5e0d326fa1df374be1e738e4fc10234
Omar Elsendiony <omar.elsendiony@hotmail.com>
2023-08-17 23:48:14 +0300

    f

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,22 +9,26 @@ int main(void)
 	int i;
 
 	printf("1 2");
-	for (i = 1; i < 101; i++)
+	for (i = 3; i < 101; i++)
 	{
 		printf(" ");
-		if (i % 3 && i % 5)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
 		else
 		{
-			if (i % 3)
+			if (i % 3 == 0)
 			{
 				printf("Fizz");
 			}
-			if (i % 5)
+			else if (i % 5 == 0)
 			{
 				printf("Buzz");
+			}
+			else
+			{
+				printf("%d",i);
 			}
 		}
 	}
