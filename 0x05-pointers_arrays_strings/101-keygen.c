3f18c34144bb6d2c3db60c33dd8cf7f96c6ac663
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-22 01:12:49 +0300

    g

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 /**
  * main - returns the alphabet
@@ -7,5 +9,35 @@
 */
 int main(void)
 {
-    int i;
+    char s[65];
+    int i, accumulative, goal, generatedNum;
+
+    i = 0;
+    goal = 0xad4;
+    accumulative = 0;
+    srand(time(NULL));
+    while (i < 64)
+    {
+        if ((goal - accumulative) > 126)
+        {
+            generatedNum = rand() % 83 + 44;
+        }
+        else if((goal - accumulative) >= 44 && (goal - accumulative) <= 126)
+        {
+            s[i] = (goal - accumulative);
+            s[i + 1] = '\0';
+            break;
+        }
+        else
+        {
+            i = 0;
+            accumulative = 0;
+            continue;
+        }
+        accumulative += generatedNum;
+        s[i] = generatedNum;
+        i += 1;
+    }
+    printf("%s", s);
+    return (0);
 }
