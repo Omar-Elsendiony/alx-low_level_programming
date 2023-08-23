1372430ed0b1b7509081daa08141aa249ba79634
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-23 21:29:46 +0300

    ww

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,11 @@ char *string_toupper(char *s)
 	int i;
 
 	i = 0;
-	i = 0;
 	while (s[i] != '\0')
-    {
-        if (s[i] >= 97 && s[i] <= 122)
-            s[i] -= 32;
-    }
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
+		i += 1;
+	}
 	return (s);
 }
