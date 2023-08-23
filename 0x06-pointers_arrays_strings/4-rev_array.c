aa9b74434cb14abd55785adb56908f5ae6df09a8
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-23 20:27:22 +0300

    b

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - returns the alphabet
+ * @a: pointer to integer
+ * @n: pointer to second number
+ * Description: The description
+ * Return: void
+*/
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	i = 0;
+	for (i = 0; i < n; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+	}
+}
