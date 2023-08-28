5c7d4ba417434592bc1e5ff9a300e33dc37805f3
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-28 20:18:46 +0300

    b

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -21,5 +21,5 @@ char *_strchr(char *s, char c)
 			}
 			i++;
 		}
-		return (0x00);
+		return (NULL);
 }
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - prints buffer in hexa
+ * @s: the address of memory to print
+ * @accept: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+		unsigned int i, count, j;
+
+		i = 0;
+		while (s[i] != '\0')
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (accept[j] == s[i])
+				{
+					count++;
+					break;
+				}
+			}
+			i++;
+		}
+		return (count);
+}
