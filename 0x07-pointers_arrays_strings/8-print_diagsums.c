3dc2ce9d938bbb41ddea6cd0b8ab50d18754d201
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-28 23:54:36 +0300

    f

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_diagsums - prints buffer in hexa
+ * @a: the address of memory to print
+ * @size: pp
+ * Return: Nothing.
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned int i, j, sum1, sum2;
+
+	i = 0;
+	for (i = 0; i < size; i++)
+	{
+		sum1 +=  *(a + (size - 1) * i + i);
+		sum2 +=  *(a + (size - 1) * i + (size - 1 - i));
+	}
+	printf("%d, %d", sum1, sum2);
+}
