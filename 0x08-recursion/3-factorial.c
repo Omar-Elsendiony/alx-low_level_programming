78e8a7211ef58b62e9e7c3113c3d2772ff309b88
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-30 22:31:02 +0300

    h

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _pow_recursion - prints buffer in hexa
- * @x: the address of memory to print
- * @y: the address of memory to print
+ * factorial - prints buffer in hexa
+ * @n: the address of memory to print
  *
  * Return: Nothing.
  */
-int _pow_recursion(int x, int y)
+int factorial(int n)
 {
-	if (y < 0)
+	if (n < 0)
 		return (-1);
-	if (y == 0)
+	if (n == 0)
 		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	return (n * _pow_recursion(n - 1));
 }
