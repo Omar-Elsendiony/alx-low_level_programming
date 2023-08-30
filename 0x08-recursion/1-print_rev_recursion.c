baa22f47090334301ba9cb8b77378a4081bdadcd
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-30 21:20:52 +0300

    f

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_utility - prints buffer in hexa
+ * @s: the address of memory to print
+ * @i: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void print_utility(char *s, int i)
+{
+	if (s[i] == '\0')
+		return;
+	print_utility(s, ++i);
+	_putchar(s[i]);
+}
+/**
+ * _puts_recursion - prints buffer in hexa
+ * @s: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	print_utility(s, 0);
+	_putchar('\n');
+}
