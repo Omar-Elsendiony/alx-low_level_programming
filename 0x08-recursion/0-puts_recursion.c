c1e2be6644e26957f4ec9cb11bcd3d40cd4254df
Omar Elsendiony <omar.sendiony@gmail.com>
2023-08-30 21:07:56 +0300

    g

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * print_utility - prints buffer in hexa
+ * @s: the address of memory to print
+ * @i: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void print_utility(char *s, int i)
+{
+	if (s[i] == '\0')
+		return;
+	_putchar(s[i]);
+	print_utility(s, i++);
+}
+/**
  * _puts_recursion - prints buffer in hexa
  * @s: the address of memory to print
  *
@@ -8,11 +22,6 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] < '\0'; i++)
-	{
-		_putchar(s[i]);
-	}
+	print_utility(s, 0);
 	_putchar('\n');
 }
