f16b849ef00f8f0ea149f43dfd18edda12bd9885
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-02 00:47:45 +0300

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - starting point
+ * Description: The description
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+}
