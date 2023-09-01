b3c74cf1a8da6f517dd8271df4b75e58555c4b2d
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-02 02:54:33 +0300

    df

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,11 @@ int convertToNumber(char *s)
 	int i, theNumber;
 
 	theNumber = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	if (s[0] == '-')
+		i = 1;
+	else
+		i = 0;
+	for (; s[i] != '\0'; i++)
 	{
 		theNumber = theNumber * 10 + (s[i] - '0');
 	}
@@ -30,14 +34,20 @@ int convertToNumber(char *s)
 */
 int main(int argc, char **argv)
 {
-	int firstArg, secondArg;
+	int firstArg, secondArg, signFirstArg, signSecondArg;
 
+	signFirstArg = 1;
+	signSecondArg = 1;
 	if (argc < 3)
 		printf("Error\n");
 	else
 	{
-		firstArg = convertToNumber(argv[1]);
-		secondArg = convertToNumber(argv[2]);
+		if (argv[1][0] == '-')
+			signFirstArg = -1;
+		if (argv[2][0] == '-')
+			signSecondArg = -1;
+		firstArg = convertToNumber(argv[1]) * signFirstArg;
+		secondArg = convertToNumber(argv[2]) * signSecondArg;
 		printf("%d\n", firstArg * secondArg);
 	}
 	return (0);
