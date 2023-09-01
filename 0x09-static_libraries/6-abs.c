887e0b7b555f223fbe6954e13b7face4d9879620
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-02 01:17:23 +0300

    f

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include "string.h"
 /**
- * abs - starting point
+ * _abs - starting point
  * @n: the character
  * Description: The description
  * Return: 0
 */
-int abs(int n)
+int _abs(int n)
 {
 	if (n < 0)
-		n *= -1;
+		n = n * -1;
 	return (n);
 }
