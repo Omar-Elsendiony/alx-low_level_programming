7bf4068262fcddb471465f223685868b5d4b5c35
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-02 05:43:49 +0300

    v

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,32 +2,33 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int utility(int money,int accumulated,int count,int coins [], int numberCoins, int dp [1000][100])
+int utility(int money, int accumulated, int count, int coins[], int numberCoins, int dp[1000])
 {
-    int i, min, ret;
+	int i, min, ret;
 
-    if (dp[accumulated][count] != -1)
-        return dp[accumulated][count];
-    min = INT_MAX;
-    if (accumulated > money)
-    {
-        dp[accumulated][count] = -1;
-        return dp[accumulated][count];      
-    }
-    if (accumulated == money)
-    {
-        dp[accumulated][count] = count;
-        return dp[accumulated][count];
-    }
-    for (i = 0; i < numberCoins; i++)
-    {
-        ret = utility(money, accumulated + coins[i], count + 1, coins, numberCoins, dp);
-        if (ret < min && ret != -1)
-        {
-            min = ret;
-        }
-    }
-    return min;
+	if (dp[accumulated] != -1)
+		return (dp[accumulated]);
+	min = INT_MAX;
+	if (accumulated > money)
+	{
+		dp[accumulated] = -1;
+		return (dp[accumulated]);
+	}
+	if (accumulated == money)
+	{
+		dp[accumulated] = count;
+		return (dp[accumulated]);
+	}
+	for (i = 0; i < numberCoins; i++)
+	{
+		ret = utility(money, accumulated + coins[i], count + 1, coins, numberCoins, dp);
+		if (ret < min && ret != -1)
+		{
+			min = ret;
+		}
+	}
+	dp[accumulated] = min;
+	return (min);
 }
 /* betty style doc for function main goes there */
 /**
@@ -39,29 +40,26 @@ int utility(int money,int accumulated,int count,int coins [], int numberCoins, i
 */
 int main(int argc, char **argv)
 {
-	int i, j, money, count;
-    int coins[] = {25, 10, 5, 2, 1};
-    int dp [1000][100];
+	int i, money, count;
+	int coins[] = {25, 10, 5, 2, 1};
+	int dp[1000];
 
-    for (i = 0; i < 1000; i++)
-    {
-        for (j = 0; j < 100; j++)
-        {
-            dp[i][j] = -1;
-        }
-    }
-    if (argc - 1 != 1)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	for (i = 0; i < 1000; i++)
+	{
+		dp[i] = -1;
+	}
+	if (argc - 1 != 1)
+	{
+		puts("Error");
+		return (1);
+	}
 	money = atoi(argv[1]);
-    if (money <= 0)
-    {
-        printf("0\n");
-        return (1);
-    }
-    count = utility(money, 0, 0, coins, 5, dp);
+	if (money <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	count = utility(money, 0, 0, coins, 5, dp);
 	printf("%d\n", count);
 	return (0);
 }
