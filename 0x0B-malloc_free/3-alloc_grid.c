c265e59f0141f151f5be2aa8146a4a4d82891f2e
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-04 17:27:35 +0300

    h

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	allocatedMemory = malloc(sizeof(int*) * height);
+	allocatedMemory = (int**)malloc(sizeof(int*) * height);
 	if (allocatedMemory == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
