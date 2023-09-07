48193e226865144ca4b6cf50a59794690dbce3e4
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-07 22:25:52 +0300

    d

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: the address of memory to print
+ * @size: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *allocatedMem;
+	int i;
+
+	i = 0;
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	allocatedMem = malloc(nmemb * size);
+	for (; i < nmemb * size; i++)
+	{
+		allocatedMem[i] = 0;
+	}
+	if (allocatedMem == NULL)
+	{
+		return (NULL);
+	}
+	return (allocatedMem);
+}
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat(NULL, "School !!!", 2);
+    concat = string_nconcat("fjfhshg", "gjg", -1);
     printf("%s\n", concat);
     free(concat);
     return (0);
