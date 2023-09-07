384029bad93002b7119d2e56d99142a7e2e10edf
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-07 22:41:40 +0300

    f

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - prints buffer in hexa
+ * @min: the address of memory to print
+ * @max: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+int *array_range(int min, int max)
+{
+	char *allocatedMem;
+	unsigned int i, diff;
+
+	if (min > max)
+		return (NULL);
+	allocatedMem = malloc(max - min + 1);
+	if (allocatedMem == NULL)
+	{
+		return (NULL);
+	}
+	diff = max - min + 1;
+	for (i = min; i <= max; i++)
+	{
+		allocatedMem[i - min] = i;
+	}
+	return (allocatedMem);
+}
