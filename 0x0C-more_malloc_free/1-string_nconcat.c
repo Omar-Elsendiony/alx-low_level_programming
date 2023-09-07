66667839b292f573dbbe1147cd2e1e78b04ba62b
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-07 21:25:47 +0300

    f

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the address of memory to print
+ * @n: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *allocatedMem;
+	unsigned int i = 0, j = 0, comparator = 0;
+
+	allocatedMem = malloc(strlen(s1) + strlen(s2) + 1);
+	if (allocatedMem == NULL)
+		return (NULL);
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	while (s1[i] != '\0')
+	{
+		allocatedMem[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0' && comparator != n)
+	{
+		allocatedMem[i] = s2[j];
+		i++;
+		j++;
+		comparator++;
+	}
+	allocatedMem[i] = '\0';
+	return (allocatedMem);
+}
