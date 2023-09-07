1fb8b423464bbec742102d841217baa7d2d60fc5
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-07 20:31:57 +0300

    b

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - prints buffer in hexa
+ * @b: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *allocatedMem;
+
+	allocatedMem = malloc(b);
+	if (allocatedMem == NULL)
+	{
+		return (98);
+	}
+	return (allocatedMem);
+}
