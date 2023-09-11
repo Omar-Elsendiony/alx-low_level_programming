5478a95e7a22a6cbb18bc119b7c4a8554e8b2ede
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-12 01:28:58 +0300

    v

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - prints buffer in hexa
+ * @d: the address of memory to print
+ * @name: the address of memory to print
+ * @owner: the address of memory to print
+ * @age: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
