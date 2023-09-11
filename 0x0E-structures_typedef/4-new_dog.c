31d7e86f53086129716d86153a86aaee3a4926bc
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-12 01:56:11 +0300

    V

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *d = malloc(sizeof(struct dog));
-
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
