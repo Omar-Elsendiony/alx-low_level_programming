7e5b39799bfdca3cfd2bb83c93b75330d51156c9
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-12 01:54:15 +0300

    b

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - prints buffer in hexa
+ * @d: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
