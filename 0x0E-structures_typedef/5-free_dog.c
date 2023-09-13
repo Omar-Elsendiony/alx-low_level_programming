5172ff346b1503e44a1278b72eb13c6b5c8025e3
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-14 02:41:29 +0300

    f

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -11,7 +11,7 @@ void free_dog(dog_t *d)
 	if (d->name != NULL)
 		free(d->name);
 	if (d->owner != NULL)
-		free(d->owner);    
+		free(d->owner);
 	if (d != NULL)
 		free(d);
 }
---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include "string.h"
+/**
+ * print_alphabet - returns the alphabet
+ * Description: The description
+ * Return: 0
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
