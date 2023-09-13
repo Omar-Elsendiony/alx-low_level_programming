2897ab3a6c6ba68cdb654f759b8dbaf71bb2cbba
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-14 02:54:15 +0300

    b

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * print_name - returns the alphabet
+ * @array: ny
+ * @size: pointer
+ * action:tt
+ * Description: The description
+ * Return: 0
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
