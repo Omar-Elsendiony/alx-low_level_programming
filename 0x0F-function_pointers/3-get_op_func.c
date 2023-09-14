9cc623984ee9c355dede50b025c77c0220197f7b
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-14 05:39:09 +0300

    f

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -40,7 +40,7 @@ int (*get_op_func(char *s))(int, int)
     i = 0;
     while (i < 6)
     {
-        if (!stringCompare(ops[i].op, s))
+        if (s != NULL && ops[i].op[0] == s[0] && ops[i].op[1] == s[1])
         {
             return (ops[i].f);
         }
