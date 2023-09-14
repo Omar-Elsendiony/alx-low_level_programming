3c9276acad4a424913268be182cdfe0fc17573fc
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-15 01:51:51 +0300

    b

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -41,7 +41,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		number = va_arg(valist, int);
 		printf("%d", number);
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 		{
 			print_seperator(separator);
 		}
