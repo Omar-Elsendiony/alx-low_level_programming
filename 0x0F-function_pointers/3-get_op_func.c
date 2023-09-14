ce97d8bc9aaa2708ec5e93c2520176060dbd2130
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-14 05:45:53 +0300

    g

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,27 +3,6 @@
 #include "function_pointers.h"
 #include "string.h"
 
-int stringCompare(char *a, char *b)
-{
-    int i;
-
-    if (a == NULL || b == NULL)
-    {
-        return 1;
-    }
-    i = 0;
-    while (a[i] != '\0' && b[i] != '\0')
-    {
-        if (a[i] != b[i])
-            break;
-        i++;
-    }
-    if (a[i] == '\0' && b[i] == '\0')
-    {
-        return (0);
-    }
-    return (1);
-}
 
 int (*get_op_func(char *s))(int, int)
 {
