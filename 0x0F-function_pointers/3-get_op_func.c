dfc0266b5f1925632b33575325e74dba4352f341
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-14 05:50:24 +0300

    g

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,27 +3,31 @@
 #include "function_pointers.h"
 #include "string.h"
 
-
+/**
+ * get_op_func - check the code
+ * @s: t
+ * Return: Always 0.
+ */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    i = 0;
-    while (i < 6)
-    {
-        if (s != NULL && ops[i].op[0] == s[0] && ops[i].op[1] == s[1])
-        {
-            return (ops[i].f);
-        }
-        i++;
-    }
-    return (NULL);
+	i = 0;
+	while (i < 6)
+	{
+		if (s != NULL && ops[i].op[0] == s[0] && ops[i].op[1] == s[1])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
