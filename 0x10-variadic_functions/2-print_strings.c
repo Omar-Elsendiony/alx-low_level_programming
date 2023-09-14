b43e697871babc091d95d0c4c7006d1e851a2cdd
Omar Elsendiony <omar.sendiony@gmail.com>
2023-09-15 01:56:17 +0300

    v

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_seperator - prints buffer in hexa
+ * @sep: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void print_seperator(const char *sep)
+{
+	int i;
+
+	i = 0;
+	while (sep[i] != '\0')
+	{
+		printf("%c", sep[i]);
+		i++;
+	}
+}
+/**
+ * print_strings - prints buffer in hexa
+ * @n: the address of memory to print
+ * @separator: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	int number;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return;
+	}
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(valist, char *));
+		if (i != (n - 1) && separator != NULL)
+		{
+			print_seperator(separator);
+		}
+	}
+	va_end(valist);
+	printf("\n");
+}
