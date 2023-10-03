998f6cbd6d76b626ccf999ed1c805519fc4c35dc
Omar Elsendiony <omar.sendiony@gmail.com>
2023-10-03 13:07:36 +0300

    p

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile - returns the alphabet
+ * @filename: pointer to second number
+ * @letters: pointer to second number
+ * Description: The description
+ * Return: void
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *myFile, currentChar;
+	int iter, resWrite;
+
+	if (filename == NULL)
+		return (0);
+	myFile = fopen(filename, O_RDONLY);
+	if (myFile == NULL)
+		return (0);
+	while (getc(myFile) != EOF && letters != iter)
+	{
+		currentChar = getc(myFile);
+		resWrite = write(1, &currentChar, 1);
+		if (resWrite == -1)
+			return (0);
+		iter++;
+	}
+	if (getc(myFile) == EOF)
+		return (iter - 1);
+	return (iter);
+}
