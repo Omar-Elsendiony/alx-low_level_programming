3de5894ef72fec0c03925c1c7769b1388c68a2ff
Omar Elsendiony <omar.sendiony@gmail.com>
2023-10-16 20:35:30 +0300

    g

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,12 +9,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	int testBit = 0x01;
+	int testBit = 0x01, numBits = 0;
+	unsigned long int temp = n;
 
-	while (n != 0)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		_putchar(n & testBit);
-		n >> 1;
+		while (temp != 0)
+		{
+			temp = temp >> 1;
+			numBits += 1;
+		}
+		numBits--;
+		while (numBits >= 0)
+		{
+			_putchar(((n >> numBits) & testBit) + '0');
+			numBits--;
+		}
 	}
-	_putchar('\n');
 }
