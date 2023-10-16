db7a5a3c60c3950cb828c0112a0716ebbe029fe9
Omar Elsendiony <omar.sendiony@gmail.com>
2023-10-16 19:43:48 +0300

    i

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 
 
 /**
- * clear_bits - writes the character c to stdout
+ * clear_bit - writes the character c to stdout
  * @n: The character to print
  * @index: The character to print
  *
@@ -13,6 +13,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int testBit = 0x01;
 	unsigned int i;
 
+	if (index > 63)
+		return (-1);
 	for (i = 0; i < index; i++)
 	{
 		testBit = testBit << 1;
